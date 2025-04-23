07948eed8f5359705180c4df57aef1874f5686bf
Bagasmaulana07 <bagasmaulanaa52@gmail.com>
2025-04-23 19:10:47 +0700

    Add files via upload

---
+++ Program IF.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	string kata;
+	cout<<"Masukan kata = HALO" <<endl;
+	cin>>kata;
+	
+	if (kata=="HALO"){
+		cout<<"Kata yang dimasukan sesuai"<<endl;
+	}else{
+		cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	}
+}
